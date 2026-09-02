3fa0812257e9ce99b5c0578b050e67f8a76b371c
JonathanH2004 <imosa253@gmail.com>
2026-09-01 21:38:18 -0600

    todo

--- PrimeCRM_Api.Infraestructure/obj/Debug/net10.0/PrimeCRM_Api.Infraestructure.AssemblyInfo.cs
+++ PrimeCRM_Api.Infraestructure/obj/Debug/net10.0/PrimeCRM_Api.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeCRM_Api.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b070a9810ce2949f78f3d9cd5e0512fd6bdb57")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeCRM_Api.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeCRM_Api.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
