535389153d1bd02d8e8079e8def3b1d4d1c1cb8f
Jonathan Humberto Orantes Portillo <jonathanhumbertoorantesportillo@MacBook-Pro-de-Jonathan.local>
2026-09-06 16:26:03 -0600

    relaciones master con demas entidades completado

--- PrimeCRM_Api.Domain/Models/Masters/PaymentMethod.cs
+++ PrimeCRM_Api.Domain/Models/Masters/PaymentMethod.cs
@@ -1,4 +1,5 @@
 ﻿using PrimeCRM_Api.Domain.Models.Masters.Enums;
+using PrimeCRM_Api.Domain.Models.Reabastecimimento;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,6 +15,9 @@ namespace PrimeCRM_Api.Domain.Models.Masters
 
         [MaxLength(4)]
         public string? Last4 { get; set; }
+
+        public ICollection<RestockOrder> RestockOrders { get; set; }
+
 
     }
 }
